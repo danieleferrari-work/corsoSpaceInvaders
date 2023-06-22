bfbe5a68807dc965d4c1b9244897533e8dc6e036
Daniele Ferrari CP <daniele.ferrari.work@gmail.com>
2023-06-22 10:40:32 +0200

    enemy are destructible

--- corsoSpaceInvaders/Assets/FINAL/Scripts/EnemyController.cs
+++ corsoSpaceInvaders/Assets/FINAL/Scripts/EnemyController.cs
@@ -4,9 +4,25 @@ public class EnemyController : MonoBehaviour
 {
     EnemiesController enemiesController;
 
-    
+    private void Awake()
+    {
+        enemiesController = FindObjectOfType<EnemiesController>();
+    }
+
     void OnDestroy()
     {
         enemiesController.enemyDefeated(this);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        BulletController bullet = other.gameObject.GetComponentInParent<BulletController>();
+        bool isPlayerBullet = bullet.tag == Const.Tags.Player;
+
+        if (isPlayerBullet && bullet != null)
+        {
+            GameObject.Destroy(bullet.gameObject);
+            GameObject.Destroy(gameObject);
+        }
     }
 }
