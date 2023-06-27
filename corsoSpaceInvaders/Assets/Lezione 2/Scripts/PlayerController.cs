898e092419cb5417392c439698c427640fa511a7
Daniele Ferrari <daniele.ferrari.work@gmail.com>
2023-06-27 12:45:13 +0200

    Handle bullet collision with player
    enemy fire rate incremented for rapid testing

--- corsoSpaceInvaders/Assets/Lezione 2/Scripts/PlayerController.cs
+++ corsoSpaceInvaders/Assets/Lezione 2/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
+    [SerializeField] int life = 3;
+
 
     void Update()
     {
@@ -13,6 +15,24 @@ public class PlayerController : MonoBehaviour
             h * movementSpeed,
             0,
             v * movementSpeed);
+    }
 
+    // // parte 1
+    // // fai vedere che questo non viene stampato
+    // void OnCollisionEnter(Collision other)
+    // {
+    //     Debug.Log($"Collision with {other.gameObject.name}");
+    // }
+
+    // parte 2 
+    // fai vedere che questo invece viene stampato
+    // perchè il bullet è Trigger
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Trigger enter with {other.gameObject.name}");
+        life--;
+
+        if (life == 0)
+            Debug.LogWarning("GAME OVER");
     }
 }
